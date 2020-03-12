d8710e7b679f40d6e9b144a3f5f112f2ed5d198f
Kerry Buckley <kerryjbuckley@gmail.com>
2020-03-12 21:17:09 +0000

    Regex solution

--- lib/word_buckets.ex
+++ lib/word_buckets.ex
@@ -1,5 +1,11 @@
 defmodule WordBuckets do
   def bucketize(text, bucket_size) do
-    []
+    if text =~ ~r/\S{#{bucket_size + 1}}/ do
+      []
+    else
+      ~r/(?=\S)(.{1,#{bucket_size}})(?<=\S)\b/
+      |> Regex.scan(text, capture: :all_but_first)
+      |> List.flatten()
+    end
   end
 end
