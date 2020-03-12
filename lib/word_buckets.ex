3414045e21f430e40f2271c33f0e3ec5d34a9449
Kerry Buckley <kerryjbuckley@gmail.com>
2020-03-12 09:36:00 +0000

    New project

---
+++ lib/word_buckets.ex
@@ -0,0 +1,18 @@
+defmodule WordBuckets do
+  @moduledoc """
+  Documentation for `WordBuckets`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WordBuckets.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/word_buckets_test.exs
@@ -0,0 +1,8 @@
+defmodule WordBucketsTest do
+  use ExUnit.Case
+  doctest WordBuckets
+
+  test "greets the world" do
+    assert WordBuckets.hello() == :world
+  end
+end
